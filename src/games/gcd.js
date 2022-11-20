ae27154ec42032998946851820e20a4c7482f8e3
EldarOpera <eldar.akhmedov.17@gmail.com>
2022-11-20 16:09:12 +0300

    Improve Cognitive Complexity

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,34 +3,33 @@ import generateRandomNumber from '../genRandomNum.js';
 
 const gcdGame = () => {
   const rules = 'Find the greatest common divisor of given numbers.';
+
   const gameResult = () => {
     const randomNum1 = generateRandomNumber(100);
     const randomNum2 = generateRandomNumber(100);
 
-    const gcdFinding = (num1, num2) => {
-      const divisors1 = [];
-      const divisors2 = [];
-      const terminal = Math.max(num1, num2);
-      for (let i = 1; i <= terminal; i += 1) {
-        if (num1 % i === 0) {
-          divisors1.push(i);
-        }
-        if (num2 % i === 0) {
-          divisors2.push(i);
-        }
-      }
+    const divisors1 = [];
+    const divisors2 = [];
+    const terminal = Math.max(randomNum1, randomNum2);
 
-      let result = 0;
-      for (let i = 1; i <= terminal; i += 1) {
-        if (divisors1.includes(i) && divisors2.includes(i)) {
-          result = result < i ? i : null;
-        }
+    for (let i = 1; i <= terminal; i += 1) {
+      if (randomNum1 % i === 0) {
+        divisors1.push(i);
       }
+      if (randomNum2 % i === 0) {
+        divisors2.push(i);
+      }
+    }
 
-      return result;
-    };
+    let result = 0;
+
+    for (let i = 1; i <= terminal; i += 1) {
+      if (divisors1.includes(i) && divisors2.includes(i)) {
+        result = result < i ? i : null;
+      }
+    }
     const question = `${randomNum1} ${randomNum2}`;
-    const correctAnswer = `${gcdFinding(randomNum1, randomNum2)}`;
+    const correctAnswer = `${result}`;
 
     return [question, correctAnswer];
   };
