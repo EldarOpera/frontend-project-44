486d7bfaadc1b716ff9f6cda93afe6e5f4049c46
EldarOpera <eldar.akhmedov.17@gmail.com>
2022-11-22 22:15:33 +0300

    Fix gcd game functionality

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -8,24 +8,23 @@ const playGcd = () => {
     const randomNumbers = [generateRandomNumber(100), generateRandomNumber(100)];
     const terminal = Math.max(randomNumbers[0], randomNumbers[1]);
     const divisors = [[], []];
-    let result = 0;
 
-    for (let divisor = 1; divisor <= terminal; i += 1) {
+    const question = `${randomNumbers[0]} ${randomNumbers[1]}`;
+    let correctAnswer = 0;
+
+    for (let divisor = 1; divisor <= terminal; divisor += 1) {
       if (randomNumbers[0] % divisor === 0) {
-        divisors[0].push(i);
+        divisors[0].push(divisor);
       }
       if (randomNumbers[1] % divisor === 0) {
-        divisors[1].push(i);
+        divisors[1].push(divisor);
       }
       if (divisors[0].includes(divisor) && divisors[1].includes(divisor)) {
-        if (result < i) { result = i; }
+        if (correctAnswer < divisor) { correctAnswer = divisor; }
       }
     }
 
-    const question = `${randomNumbers[0]} ${randomNumbers[1]}`;
-    const correctAnswer = `${result}`;
-
-    return [question, correctAnswer];
+    return [question, `${correctAnswer}`];
   };
 
   gameEngine(rules, startRound);
