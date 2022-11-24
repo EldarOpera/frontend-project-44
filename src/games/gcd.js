c7605c47a9f228dcff53e05ee96db54e60f1bb60
EldarOpera <eldar.akhmedov.17@gmail.com>
2022-11-24 16:26:42 +0300

    Modify gcd.js functionality'

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,4 +1,4 @@
-import gameEngine from '../index.js';
+import startGame from '../index.js';
 import generateRandomNumber from '../genRandomNum.js';
 
 const playGcd = () => {
@@ -9,25 +9,29 @@ const playGcd = () => {
     const terminal = Math.max(randomNumbers[0], randomNumbers[1]);
     const divisors = [[], []];
 
-    const question = `${randomNumbers[0]} ${randomNumbers[1]}`;
-    let correctAnswer = 0;
-
-    for (let divisor = 1; divisor <= terminal; divisor += 1) {
-      if (randomNumbers[0] % divisor === 0) {
-        divisors[0].push(divisor);
+    const findGcd = () => {
+      let result = 0;
+      for (let divisor = 1; divisor <= terminal; divisor += 1) {
+        if (randomNumbers[0] % divisor === 0) {
+          divisors[0].push(divisor);
+        }
+        if (randomNumbers[1] % divisor === 0) {
+          divisors[1].push(divisor);
+        }
+        if (divisors[0].includes(divisor) && divisors[1].includes(divisor)) {
+          if (result < divisor) { result = divisor; }
+        }
       }
-      if (randomNumbers[1] % divisor === 0) {
-        divisors[1].push(divisor);
-      }
-      if (divisors[0].includes(divisor) && divisors[1].includes(divisor)) {
-        if (correctAnswer < divisor) { correctAnswer = divisor; }
-      }
+      return String(result);
     }
 
-    return [question, `${correctAnswer}`];
+    const question = `${randomNumbers[0]} ${randomNumbers[1]}`;
+    const correctAnswer = findGcd();
+
+    return [question, correctAnswer];
   };
 
-  gameEngine(rules, startRound);
+  startGame(rules, startRound);
 };
 
 export default playGcd;
