7dec2a41b7a5db5378fccf6514535385c2e0a6a3
EldarOpera <eldar.akhmedov.17@gmail.com>
2022-11-24 15:54:46 +0300

    Modify calc.js functionality

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,37 +1,44 @@
-import gameEngine from '../index.js';
+import startGame from '../index.js';
 import generateRandomNumber from '../genRandomNum.js';
 
 const playCalc = () => {
   const rules = 'What is the result of the expression?';
+  const operators = ['+', '-', '*'];
 
   const startRound = () => {
     const randomNum1 = generateRandomNumber(1, 36);
     const randomNum2 = generateRandomNumber(1, 36);
 
-    const operators = ['+', '-', '*'];
     const randomOperator = operators[generateRandomNumber(0, operators.length - 1)];
 
-    const question = `${randomNum1} ${randomOperator} ${randomNum2}`;
+    const calculate = (operator, num1, num2) => {
+      let result;
+      switch (operator) {
+        case '+':
+          result = `${num1 + num2}`;
+          break;
+        case '-':
+          result = `${num1 - num2}`;
+          break;
+        case '*':
+          result = `${num1 * num2}`;
+          break;
+        /* case '/':
+          result = `${num1 / num2}`;
+          break; */
+        default:
+          result = null;
+      }
+      return result;
+    };
 
-    let correctAnswer;
-    switch (randomOperator) {
-      case '+':
-        correctAnswer = `${randomNum1 + randomNum2}`;
-        break;
-      case '-':
-        correctAnswer = `${randomNum1 - randomNum2}`;
-        break;
-      case '*':
-        correctAnswer = `${randomNum1 * randomNum2}`;
-        break;
-      default:
-        correctAnswer = null;
-    }
+    const question = `${randomNum1} ${randomOperator} ${randomNum2}`;
+    const correctAnswer = calculate(randomOperator, randomNum1, randomNum2);
 
     return [question, correctAnswer];
   };
 
-  gameEngine(rules, startRound);
+  startGame(rules, startRound);
 };
 
 export default playCalc;
