912ed02e8ad06e2c85e66550c439b44eab8f99fd
EldarOpera <eldar.akhmedov.17@gmail.com>
2022-11-26 14:06:05 +0300

    Modify calc game logic (calc.js)

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,30 +1,32 @@
 import startGame from '../index.js';
 import generateRandomNumber from '../genRandomNum.js';
 
+const rules = 'What is the result of the expression?';
+
+const operators = ['+', '-', '*'];
+
+const calculate = (operator, num1, num2) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return `Unknown operator: ${operator}`;
+  }
+};
+
 const playCalc = () => {
-  const rules = 'What is the result of the expression?';
-  const operators = ['+', '-', '*'];
 
   const startRound = () => {
     const randomNum1 = generateRandomNumber(1, 36);
     const randomNum2 = generateRandomNumber(1, 36);
     const randomOperator = operators[generateRandomNumber(0, operators.length - 1)];
 
-    const calculate = (operator, num1, num2) => {
-      switch (operator) {
-        case '+':
-          return String(num1 + num2);
-        case '-':
-          return String(num1 - num2);
-        case '*':
-          return String(num1 * num2);
-        default:
-          return null;
-      }
-    };
-
     const question = `${randomNum1} ${randomOperator} ${randomNum2}`;
-    const correctAnswer = calculate(randomOperator, randomNum1, randomNum2);
+    const correctAnswer = String(calculate(randomOperator, randomNum1, randomNum2));
 
     return [question, correctAnswer];
   };
