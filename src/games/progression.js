3e84f19709c1dbf245c343b73638b6f3a29e7cbf
EldarOpera <eldar.akhmedov.17@gmail.com>
2022-11-30 15:05:32 +0300

    Update progression game logic

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,37 +3,28 @@ import generateRandomNumber from '../genRandomNum.js';
 
 const description = 'What number is missing in the progression?';
 
-const createProgression = (start, toSkip) => {
+const createProgression = (begin, toSkip, length) => {
   const progression = [];
-  for (let i = 0, j = start; i < 10; i += 1, j += toSkip) {
-    progression.push(j);
+  const terminal = begin + (length * toSkip);
+  for (let i = begin; i < terminal; i += toSkip) {
+    progression.push(i);
   }
 
   return progression;
-};
-
-const hideNumber = (progression, toHide) => {
-  const preparedProgression = progression.slice(0);
-  for (let i = 0; i < preparedProgression.length; i += 1) {
-    if (i === toHide) {
-      preparedProgression[i] = '..';
-    }
-  }
-
-  return preparedProgression;
 };
 
 const playProgression = () => {
   const startRound = () => {
     const initialNum = generateRandomNumber(1, 16);
     const numToSkip = generateRandomNumber(1, 6);
-    const numToHide = generateRandomNumber(0, 10);
+    const maxLength = generateRandomNumber(7, 10) + 1;
+    const indexToHide = generateRandomNumber(0, maxLength - 1);
 
-    const progression = createProgression(initialNum, numToSkip);
-    const question = hideNumber(progression, numToHide).join(' ');
-    const correctAnswer = String(progression[numToHide]);
+    const progression = createProgression(initialNum, numToSkip, maxLength);
+    const correctAnswer = String(progression[indexToHide]);
+    progression[indexToHide] = '..';
 
-    return [question, correctAnswer];
+    return [progression.join(' '), correctAnswer];
   };
 
   startGame(description, startRound);
